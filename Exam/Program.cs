3fa1e0d14e68a920ee3b55901b3968fca0e305e1
Anuar <beisekeyev.a.1992.ast@gmail.com>
2021-02-18 20:28:59 +0600

    added exam

---
+++ Exam/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NLog;
+using NLog.Config;
+
+
+namespace Exam
+{
+    class Program
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+            logger.Trace("trace message");
+            logger.Debug("debug message");
+            logger.Info("info message");
+            logger.Warn("warn message");
+            logger.Error("error message");
+            logger.Fatal("fatal message");
+            
+            
+        }
+    }
+}
---
+++ LoggerService/LogService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NLog;
+using NLog.Config;
+
+namespace LoggerService
+{
+    
+    public class LogService 
+    {
+        //private static Logger logger;
+
+        //public void LoggingMethod()
+        //{
+        //    logger.Trace("trace message");
+        //    logger.Debug("debug message");
+        //    logger.Info("info message");
+        //    logger.Warn("warn message");
+        //    logger.Error("error message");
+        //    logger.Fatal("fatal message");
+        //}
+
+    }
+}
